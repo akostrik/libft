e46e4f08057b355da4d262fa7a809572bde80992
Anna KOSTRIKOVA <akostrik@e3r2p3.clusters.42paris.fr>
2022-11-10 13:28:53 +0100

    ***

--- rendu/ft_isascii.c
+++ rendu/ft_isascii.c
@@ -6,9 +6,15 @@
 /*   By: akostrik <akostrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:21:18 by akostrik          #+#    #+#             */
-/*   Updated: 2022/11/09 13:44:46 by akostrik         ###   ########.fr       */
+/*   Updated: 2022/11/10 13:25:36 by akostrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+// tests for an ASCII character (0 to 127) ?
+// checks whether c is a 7-bit unsigned char value that fits into the ASCII set
+// возвращает ненулевое знач, если c от 0 до 0x7F, )0x7F =7*16+15=255 
+// POSIX 1003.1 OB XSI. isascii obsolescent
+// macros
 
 int	ft_isascii(int c)
 {
--- rendu/main.c
+++ rendu/main.c
@@ -3,49 +3,42 @@
 #include "libft.h"
 
 // The standards : c = EOF / a value that is representable in unsigned char
+// c = an unsigned char or EOF, isascii undefined behavior for other values 
 // If the argument c is of type char, it must be cast to unsigned char
-// res = toupper((unsigned char) c)
-// определена только если isascii(c) равно true или c = EOF
 // isalpha - макрокоманда
+// определена только если isascii(c) равно true или c = EOF
 // isalpha доступна в UNIX, поддерживается стандартом ANSI C
-// аргумент int, его значение должно принадлежать диапазону 
-// значений типа unsigned char, или же быть равным значению макроконстанты 
-// EOF. Если аргумент имеет любое другое значение, поведение функций не определено
 
-/// почему ilsapha макрос
 
-void print_test(char c, int(*f1)(int), int(*f2)(int))
+
+// почему ilsapha макрос
+// 	print_test("isascii",'*',isascii,ft_isascii) ?
+
+void print_test(char *func_name, char c, int(*f1)(int), int(*f2)(int))
 {
-	printf("? %s\n",int(*f1));
-	printf("isascii(%c %d %x)\t%d  \t%d\n",c,c,c,f1(c),f2(c)); //
+	//printf("? %s\n",int(*f1));
+	printf("%s(%1c %2d %2x)\t%d  \t%d\n",func_name,c,c,c,f1(c),f2(c)); //
 }
 
 int main(void)
 {
-	int c; // an unsigned char or EOF
-
 	printf("           \t\tlibc\tmy_func !\n");
-	printf("isalpha(%c)\t\t%d  \t%d\n",c,isalpha(c),ft_isalpha(c));
-	print_test('a',isalpha,ft_isalpha);
-	c = '*';
-	printf("isalpha(%c)\t\t%d  \t%d\n",c,isalpha(c),ft_isalpha(c));
-	c = '3';
-	printf("isdigit(%c)\t\t%d  \t%d\n",c,isdigit(c),ft_isdigit(c));
-	c = '0';
-	printf("isdigit(%c)\t\t%d  \t%d\n",c,isdigit(c),ft_isdigit(c));
-	c = 'a';
-	printf("isdigit(%c)\t\t%d  \t%d\n",c,isdigit(c),ft_isdigit(c));
-	c = 'a';
-	printf("isalnum(%c)\t\t%d  \t%d\n",c,isalnum(c),ft_isalnum(c));
-	c = '0';
-	printf("isalnum(%c)\t\t%d  \t%d\n",c,isalnum(c),ft_isalnum(c));
-	c = '0';
-	printf("isascii(%c %d %x)\t%d  \t%d\n",c,c,c,isascii(c),ft_isascii(c));
-	c = '#';
-	printf("isascii(%c %d %x)\t%d  \t%d\n",c,c,c,isascii(c),ft_isascii(c));
-	c = EOF;
-	printf("isascii(%c %d %x)\t%d  \t%d\n",c,c,c,isascii(c),ft_isascii(c));
-	c = NULL;
-	printf("isascii(NULL %d %x)\t%d  \t%d\n",c,c,isascii(c),ft_isascii(c));
+	print_test("isalpha",'a',isalpha,ft_isalpha);
+	print_test("isalpha",'*',isalpha,ft_isalpha);
+	print_test("isdigit",'*',isdigit,ft_isdigit);
+	print_test("isdigit",'0',isdigit,ft_isdigit);
+	print_test("isalnum",'0',isalnum,ft_isalnum);
+	print_test("isalnum",'*',isalnum,ft_isalnum);
 
+	//print_test("isascii",'*',isascii,ft_isascii); ?
+	int c = '#';
+	printf("isascii(%1c %2d %2x)\t%d  \t%d\n",c,c,c,isascii(c),ft_isascii(c));
+	c = EOF;
+	printf("isascii(%1c %2d %2x)\t%d  \t%d   // EOF\n",c,c,c,isascii(c),ft_isascii(c));
+	c = NULL;
+	printf("isascii(%2c %2d %2x)\t%d  \t%d\n",c,c,c,isascii(c),ft_isascii(c));
+	c = 0x7F;
+	printf("isascii(%2c %2d %2x)\t%d  \t%d\n",c,c,c,isascii(c),ft_isascii(c));
+	c = 0xF0;
+	printf("isascii(%c %2d %2x)\t%d  \t%d   // 0xF0 = 256\n",c,c,c,isascii(c),ft_isascii(c));
 }
