c24a3d4c22e6c9b892d5b8c2e0467e70eb874d3f
ann <akostrik@e3r7p7.clusters.42paris.fr>
2022-11-25 15:25:29 +0100

    *

--- workspace/ft_strdup.c
+++ workspace/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: akostrik <akostrik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/07 14:21:18 by akostrik          #+#    #+#             */
-/*   Updated: 2022/11/21 18:10:32 by akostrik         ###   ########.fr       */
+/*   Updated: 2022/11/25 15:25:16 by akostrik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,12 @@ Memory for the new string is obtained with malloc(3), can be freed with free(3)
 On success, returns a pointer to the duplicated string
 Returns NULL if insufficient memory was available, errno = the cause of the error
 ERRORS : ENOMEM Insufficient memory available to allocate duplicate string.
-*/
 
-// 1.OK 2.MOK 3.OK 4.MOK 
+Le standard Unix98 réclame que malloc() remplisse errno = ENOMEM en cas d'échec
+La glibc suppose qu'il en est ainsi. 
+Si vous utilisez une implémentation personnelle de malloc() qui ne définit 
+pas errno, certaines routines de bibliothèques peuvent échouer sans donner 
+de raison dans errno.*/
 
 #include "libft.h"
 
@@ -29,10 +32,7 @@ char	*ft_strdup(const char *s)
 
 	res_str = (char *)malloc((ft_strlen(s) + 1) * sizeof(char));
 	if (res_str == NULL)
-	{
-		errno = ENOMEM;
 		return (NULL);
-	}
 	i = 0;
 	while (s[i] != '\0')
 	{
